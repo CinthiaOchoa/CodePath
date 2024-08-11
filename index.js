8a6d10ee379425e72660fd3874e5b498bfde7f1b
Faizan Khan <108194702+jellyfishing2346@users.noreply.github.com>
2024-08-11 17:30:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ contributionsCard.innerHTML = `${totalContributions.toLocaleString()}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
-const totalRaised = GAMES_JSON.reduce((total, game) => total + game.backers, 0);
+const totalRaised = GAMES_JSON.reduce((total, game) => total + game.pledged, 0);
 
 // set inner HTML using template literal
 raisedCard.innerHTML = `${totalContributions.toLocaleString()}`;
