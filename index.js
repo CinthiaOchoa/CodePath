bd0837b274352e96b73c0ec603fc5d9831eb504a
Anna-Maria13 <fostera2017@fau.edu>
2025-01-13 19:07:31 -0500

    updte index.js file

--- index.js
+++ index.js
@@ -234,17 +234,17 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 
 // use destructuring and the spread operator to grab the first and second games
 
-const [topGame, runnerUpgame, ...otherGames] = sortedgames;
+const [topGame, runnerUpGame, ...otherGames] = sortedgames;
 
 //create a new element to hold the name of the top pledge game, then append it to the correct element
 
   const topGameElement = document.createElement("p");
-  topGameElement.textContent = 'Top Game: ${topGame.name}';
+  topGameElement.textContent = topGame.name;
   firstGameContainer.appendChild(topGameElement);
   
 
 // do the same for the runner up item
 
 const runnerUpGameElement = document.createElement("p");
-runnerUpGameElement.textContent = 'Runner Up: ${runnerUpGame.name}';
+runnerUpGameElement.textContent = runnerUpGame.name;
 secondGameContainer.appendChild(runnerUpGameElement);
